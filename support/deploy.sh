299f1746657f563ffcee2f1ae4bbdb2b971ccdc6
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-12-07 19:27:15 +0100

    Minor improvements to deploy script
    - Change directory to src before applying migrations
    - call git merge with no arguments (that will merge upstream branch)
    
    Change-Id: I34115639ef4889c5f9ca35f03494ad85631ccbae

--- support/deploy.sh
+++ support/deploy.sh
@@ -10,8 +10,9 @@ fi
 echo "Using branch $branch for deployment."
 cd `dirname "$0"`/..
 git fetch --all
-git merge $branch --ff-only
+git merge --ff-only
 echo "Now running database migrations.."
+cd src
 flask db upgrade
 echo "...all done! Starting webservice!"
 webservice --backend=kubernetes python3.5 start
