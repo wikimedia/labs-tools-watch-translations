3b958ac787a0b157d099885c201f8697e20aafb9
Ben Houghton <bennixerus@gmail.com>
2019-12-07 14:34:50 +0000

    Add deployment script
    Script will automatically pull latest changes (of selected branch), run database migrations and
    then restart the webservice.
    
    Bug: T240001
    Change-Id: I64f4bb477f2746e4a8b508ff10e68fbc2e4050ba

---
+++ support/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+echo "Deploying project Watch-Translations"
+echo "Stopping webservice.."
+webservice stop
+branch="master"
+if test -f "~/branch"; then
+    branch=`cat ~/branch`
+fi
+echo "Using branch $branch for deployment."
+cd `dirname "$0"`/..
+git fetch --all
+git merge $branch --ff-only
+echo "Now running database migrations.."
+flask db upgrade
+echo "...all done! Starting webservice!"
+webservice --backend=kubernetes python3.5 start
