556746f24e4fa272c3fa9429319e55d4877e4489
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-01-21 21:49:38 +0100

    deploy.sh: Improve the input a bit
    Change-Id: If03222d3e0aa8ed1b9771af08434b4fe05523410

--- support/deploy.sh
+++ support/deploy.sh
@@ -3,8 +3,6 @@ set -e
 
 echo "Deploying project Watch-Translations"
 cd ~/www/python
-
-echo "Stopping webservice.."
 webservice stop
 
 branch="master"
@@ -15,8 +13,7 @@ fi
 echo "Using branch $branch for deployment."
 git pull --ff-only
 
-echo "Now running database migrations.."
 toolforge-jobs run db-upgrade-$$ --command 'cd ~/src && ~/venv/bin/flask db upgrade' --image tf-python39 --wait
 
-echo "...all done! Starting webservice!"
 webservice --backend=kubernetes python3.9 start
+echo "...all done!"
