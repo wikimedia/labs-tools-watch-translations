7f6e2e25d752376fcbb43eec4555ef1a71961efe
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-01-21 21:46:43 +0100

    deploy.sh: Update for the k8s era
    Bug: T320134
    Change-Id: I9ca2e5c1837fba03b5a05e531aae6da8e3fda368

--- support/deploy.sh
+++ support/deploy.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
+
 echo "Deploying project Watch-Translations"
+cd ~/www/python
+
 echo "Stopping webservice.."
 webservice stop
+
 branch="master"
 if test -f "~/branch"; then
     branch=`cat ~/branch`
 fi
+
 echo "Using branch $branch for deployment."
-cd `dirname "$0"`/..
-git fetch --all
-git merge --ff-only
+git pull --ff-only
+
 echo "Now running database migrations.."
-cd src
-flask db upgrade
+toolforge-jobs run db-upgrade-$$ --command 'cd ~/src && ~/venv/bin/flask db upgrade' --image tf-python39 --wait
+
 echo "...all done! Starting webservice!"
 webservice --backend=kubernetes python3.9 start
