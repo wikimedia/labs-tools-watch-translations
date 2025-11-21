de9151c10a97d038f13eaa8e34764ff1e74d637d
Martin Urbanec <martin.urbanec@wikimedia.cz>
2025-11-21 16:47:21 +0000

    Fix deployment script
    Change-Id: Ib81943b5b52fbace8dd8736c0c049dc274ee2a7a

--- support/deploy.sh
+++ support/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+export IMAGE="python3.13"
+
 echo "Deploying project Watch-Translations"
 cd ~/www/python
-webservice stop
+toolforge webservice stop
 
 branch="master"
 if test -f "~/branch"; then
@@ -13,8 +15,8 @@ fi
 echo "Using branch $branch for deployment."
 git pull --ff-only
 
-toolforge-jobs run venv-upgrade-$$ --command 'cd ~/watch-translations && venv/bin/pip install -Ur support/requirements.txt' --image=tf-python39 --wait
-toolforge-jobs run db-upgrade-$$ --command 'cd ~/src && ~/venv/bin/flask db upgrade' --image tf-python39 --wait
+toolforge jobs run venv-upgrade-$$ --command 'cd ~/watch-translations && venv/bin/pip install -Ur support/requirements.txt' --image "$IMAGE" --wait
+toolforge jobs run db-upgrade-$$ --command 'cd ~/src && ~/venv/bin/flask db upgrade' --image "$IMAGE" --wait
 
-webservice --backend=kubernetes python3.9 start
+toolforge webservice "$IMAGE" start
 echo "...all done!"
